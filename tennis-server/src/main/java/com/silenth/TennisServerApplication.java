bff6ab3854645e5bcdcee50801fec542d007ddce
Dan Hrivnak <dhrivnak@gmail.com>
2015-03-22 22:46:50 -0500

    Hello world demo.

--- tennis-server/src/main/java/com/silenth/TennisServerApplication.java
+++ tennis-server/src/main/java/com/silenth/TennisServerApplication.java
@@ -1,5 +1,7 @@
 package com.silenth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,7 @@ public class TennisServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TennisServerApplication.class, args);
+        Logger logger = LoggerFactory.getLogger(TennisServerApplication.class);
+        logger.info("Server started!");
     }
 }
---
+++ tennis-server/src/main/java/com/silenth/web/GreetingController.java
@@ -0,0 +1,16 @@
+package com.silenth.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name); // Makes this accessible to the view template.
+        return "greeting"; // This is the name of a view.
+    }
+}
